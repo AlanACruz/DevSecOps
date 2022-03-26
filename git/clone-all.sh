6cb45fa9344160b940ebf42caa4a59525e32d9ff
Alan Cruz <cruz.alan@gmail.com>
2022-03-25 22:52:31 -0400

    standard message header

--- git/clone-all.sh
+++ git/clone-all.sh
@@ -6,11 +6,17 @@ mkdir -p ~/git;
 # Switch to ~/git folder
 pushd ~/git > /dev/null;
 
-# Curl GitHub API for URLs and Pass to Git Clone via Xargs
-curl "https://api.github.com/users/AlanACruz/repos" |
+# Curl GitHub API for URLs
+repo_list=$(curl "https://api.github.com/users/AlanACruz/repos" |
   grep -e 'ssh_url*' |   
-  cut -d \" -f 4 |
-  xargs -L1 git clone;
+  cut -d \" -f 4);
+
+# Clone Loop
+for repo in ${repo_list}; 
+do 
+  echo "--------------- ${repo} ----------------";
+  git clone ${repo};
+done; 
 
 # Switch folder back
 popd > /dev/null;
