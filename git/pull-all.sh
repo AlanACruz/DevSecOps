ddba0eb26ca40024fef3b081ad74ac1d2459a98d
Alan Cruz <cruz.alan@gmail.com>
2022-03-25 22:13:06 -0400

    adding --all

--- git/pull-all.sh
+++ git/pull-all.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 # pull everything under the ~/git folder
-pushd ~/git  > /dev/null;
+pushd ~/git > /dev/null;
 
 for repo in $(ls); do 
-  echo "----- ${repo} ------";
+  echo "--------------- ${repo} ----------------";
   pushd ~/git/${repo} > /dev/null;
-  git pull;
-  popd  > /dev/null; 
+  git pull --all;
+  popd > /dev/null; 
 done; 
 
-echo "----------------------------------------";
-popd  > /dev/null;
+popd > /dev/null;
